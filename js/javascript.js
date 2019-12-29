76d302065d58643bd197eaa52c459d1f8f0fa599
AmethystSword <amethystsword12@gmail.com>
2019-12-29 17:48:50 +0100

    new commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,25 @@
 let canvas = document.getElementById('gameScreen');
 let ctx = canvas.getContext('2d');
 
+let scriptPaddle = document.createElement('script');
+scriptPaddle.src = 'js/paddle.js';
+document.body.appendChild(scriptPaddle);
+
 canvas.width = '1100';
 canvas.height = '500';
 
 let ballRadius = 15;
 
 let paddleHeight = 20;
-let paddleWidth = canvas.width;
+let paddleWidth = 200;
 
 let x = canvas.width / 2; //x pozice micku
 let y = canvas.height - paddleHeight - 20 - ballRadius; //y pozice micku
-let dx = 3;
-let dy = -3;
-
-let speed = 10; //sec
+let dx = 4;
+let dy = -4;
 
 let ballColor = undefined;
+let frameStart = 0;
 
 const colors = ['green', 'blue', 'violet', 'yellow', 'red', 'pink', 'black', 'orange'];
 
@@ -26,8 +29,8 @@ let leftArrowPressed = false;
 let paddleX = (canvas.width / 2) - paddleWidth / 2;
 let paddleY = canvas.height - paddleHeight - 20;
 
-let brickColumnCount = 10; //pocet sloupecku
-let brickRowCount = 7; //pocet radku
+let brickColumnCount = 3; //pocet sloupecku
+let brickRowCount = 3; //pocet radku
 let brickPadding = 5; //odstup mezi kvadriky
 let brickOffsetTop = 20; // odstup od horni steny
 let brickSideOffset = 20; //odstup od bocni steny
@@ -38,13 +41,29 @@ let bricks = [];
 for (let c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
     for (let r = 0; r < brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0, status: 1};
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
 
+let score = 0;
+let lives = 3;
 
 function randomColor() {
     ballColor = colors[Math.round(Math.random() * colors.length)];
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+    drawBricks();
+    paddleLogic();
+    ballMovement();
+    drawScore(); //score is up to date
+    drawLives();
+    collisionDetection();
+    deadEnd();
+    requestAnimationFrame(draw);
 }
 
 function drawBall() {
@@ -59,16 +78,54 @@ function drawBall() {
     }
     ctx.fill();
     ctx.closePath();
-
 }
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBricks();
-    drawBall();
-    drawPaddle();
-    collisionDetection();
+function drawPaddle() {         //PADDLE
+    ctx.beginPath();
+    ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
+    ctx.fillStyle = 'green';
+    ctx.fill();
+    ctx.closePath();
+}
 
+// status 1 = paint the brick
+// status 0 = the brick was hit by a ball, delete it
+function drawBricks() {         //BRICKS
+    for (c = 0; c < brickColumnCount; c++) {
+        for (r = 0; r < brickRowCount; r++) {
+
+            if (bricks[c][r].status == 1) {
+                let brickX = (c * (brickWidth + 2 * brickPadding)) + brickSideOffset + brickPadding;
+                let brickY = (r * (brickHeight + 2 * brickPadding)) + brickOffsetTop + brickPadding;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+function paddleLogic() {        //PADDLE
+    //paddle logic
+    if (rightArrowPressed) {
+        paddleX += 8;
+        if (paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    if (leftArrowPressed) {
+        paddleX -= 8;
+        if (paddleX < 0) {
+            paddleX = 0;
+        }
+    }
+}
+
+function ballMovement() {       //BALL
     //BOUNCING 
     if (y - ballRadius <= 0) { //top wall
         dy = -dy;
@@ -80,39 +137,82 @@ function draw() {
         randomColor();
     }
 
-    //GAME OVER
-    if ((x > paddleX + paddleWidth + ballRadius || x + ballRadius < paddleX) && y - 2 * ballRadius >= canvas.height) {
-        //GAME OVER
-        alert('GAME OVER');
-        clearInterval(interval); //stop the time
-        document.location.reload();
-
-    }
-
-    //paddle collision detection
+    //PADDLE COLLISION DETECTION
     if ((x + ballRadius <= paddleX + paddleWidth && x + ballRadius >= paddleX) && (y + ballRadius >= paddleY && y + ballRadius <= paddleY + dy)) {
         dy = -dy;
     }
-    paddleLogic(); 
+
     x += dx;
     y += dy;
-
 }
 
-function drawForTheFirstTime() {
-    drawBricks();
-    drawBall();
-    drawPaddle();
+function drawScore() {
+    ctx.font = '16px Arial';
+    ctx.fillStyle = 'black';
+    ctx.fillText(`Score: ${score}`, 8, 20); //posledni dva parametry jsou souradnice
+}
+
+function drawLives() {
+    ctx.font = '16px Arial';
+    ctx.fillStyle = 'black';
+    ctx.fillText(`Lives: ${lives}`, canvas.width - 65, 20);
+}
+
+function collisionDetection() {
+    for (c = 0; c < brickColumnCount; c++) {
+        for (r = 0; r < brickRowCount; r++) {
+            let b = bricks[c][r]; //konkretni brick konkretniho sloupecku
+
+            if (b.status == 1) {
+                if ((x + ballRadius >= b.x && x - ballRadius <= b.x + brickWidth) && (y - ballRadius >= b.y && y - ballRadius <= b.y + brickHeight)) { ///:::DGSFGFDSH
+                    dy = -dy;
+                    b.status = 0; //uz se nevykresli znova
+                    score++; //score se zvetsi o 1
+                    if (score == brickColumnCount * brickRowCount) {
+                        cancelAnimationFrame(draw);
+                        ctx.clearRect(0, 0, canvas.width, canvas.height);
+                        drawBricks();
+                        drawBall();
+                        drawPaddle();
+                        drawScore();
+                        setTimeout(function() {
+                            alert('YOU WIN! You have managed to destroy all the bricks!');
+                            document.location.reload();
+                        }, 100);
+                    }
+                }
+            }
+        }
+    }
+}
+
+function deadEnd() { //GAMEOVER
+    if ((x > paddleX + paddleWidth + ballRadius || x + ballRadius < paddleX) && y - 2 * ballRadius >= canvas.height) {
+        //GAME OVER
+        if (!lives) {
+            //GAME OVER
+            alert('GAME OVER');
+            cancelAnimationFrame(draw);
+            document.location.reload();
+        }
+        else { //restart
+            lives--;
+            x = canvas.width / 2;
+            y = canvas.height - paddleHeight - 20 - ballRadius;;
+            dx = 3;
+            dy = -3;
+            paddleX = (canvas.width / paddleWidth) / 2;
+        }
+    }
 }
 
 document.addEventListener('keydown', function (e) {
     if (e.keyCode === 13 || e.keyCode === 32) {
-        let interval = setInterval(draw, speed); // every 10 ms
+        frameStart == 1;
     }
-})
-document.onload = function() {
-    drawForTheFirstTime();
-};
+}, { once: true });
+
+draw();
 
 //what to do next
     // = speed the ball up
--- js/paddle.js
+++ js/paddle.js
@@ -1,6 +1,7 @@
 
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
+document.addEventListener('mousemove', mouseMoveHandler, false);
 
 function keyDownHandler(e) { // e = event - v event parametru jsou informace o udalosti
     if (e.key == "Right" || e.key == "ArrowRight" || e.keyCode == 39) { // pri stisku sipek jsou promenne pro stisk sipek 'true'
@@ -20,28 +21,16 @@ function keyUpHandler(e) {
     }
 }
 
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
-    ctx.fillStyle = 'green';
-    ctx.fill();
-    ctx.closePath();
+function mouseMoveHandler(e) {
+    let relativeX = e.clientX - canvas.offsetLeft;
+    //canvas.offsetLeft = distance between the left edge of the canvas and left edge of the viewport
+    //e.clientX = horizontal position of mouse in viewport
+    if (relativeX >= 0 + paddleWidth/2 && relativeX <= canvas.width - paddleWidth/2) {
+        paddleX = relativeX - paddleWidth/2;
+    }
 }
- function paddleLogic() {
-      //paddle logic
-      if (rightArrowPressed) {
-        paddleX += 8;
-        if (paddleX + paddleWidth > canvas.width) {
-            paddleX = canvas.width - paddleWidth;
-        }
-    }
-    if (leftArrowPressed) {
-        paddleX -= 8;
-        if (paddleX < 0) {
-            paddleX = 0;
-        }
-    }
- }
+
+
 
 
 
